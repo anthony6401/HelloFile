a65f372bd48f11c859d37d30027535b98fa40ec9
Anthony Lie <e0407672@u.nus.edu>
2020-09-20 18:30:20 +0800

    Delete address field

--- src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandIntegrationTest.java
@@ -28,6 +28,7 @@ public class AddCommandIntegrationTest {
     @Test
     public void execute_newPerson_success() {
         Person validPerson = new PersonBuilder().build();
+        System.out.println(validPerson);
 
         Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
         expectedModel.addPerson(validPerson);
